42a13b935f06e72e1d75ded5834d1da7dc945b38
Alexander Gonçalves Ferreira <61751336+Goncalves-Alexander@users.noreply.github.com>
2020-03-09 20:55:55 -0300

    Codigo Lógico do Arduino - Bob Pepper
    Aqui será listado o código atual para execução das atividades dos sensores

---
+++ sketch_mar03a.ino
@@ -0,0 +1,87 @@
+//int pinoSensor = 5;
+int receptor = 0;
+float temperatura = 0;
+int registro = 0;
+String teste = "";
+String pimenta = "";
+
+void setup() {
+ 
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+      receptor = analogRead(5);
+      temperatura = (receptor * 0.00488) * 100;
+      pimenta = "annuum";
+      registro++;
+      if(pimenta == "annuum"){
+        if(temperatura >= 21 && temperatura <= 27){
+          teste = "Temperatura Ideal, mantenha-na!";
+          Serial.print("Temperatura: ");
+          Serial.print(temperatura);
+          Serial.print(" " + teste);
+          Serial.print(", ");
+          Serial.println(registro);    
+        }
+        else{
+          if(temperatura > 27){
+            teste = "Temperatura acima do ideal, abaixe-a";
+            Serial.print("Temperatura: ");
+            Serial.print(temperatura);
+            Serial.print(" " + teste);
+            Serial.print(", ");
+            Serial.println(registro);
+          }
+          else{
+            if(temperatura < 21){
+              teste = "Temperatura abaixo do ideal, aumente-a";
+              Serial.print("Temperatura: ");
+              Serial.print(temperatura);
+              Serial.print(" " + teste);
+              Serial.print(", ");
+              Serial.println(registro);
+            }
+          }
+        }
+      }else{
+          if(pimenta == ""){
+        if(temperatura >= 25 && temperatura <= 30){
+          teste = "Temperatura Ideal, mantenha-na!";
+          Serial.print("Temperatura: ");
+          Serial.print(temperatura);
+          Serial.print(" " + teste);
+          Serial.print(", ");
+          Serial.println(registro);  
+        }
+        else{
+          if(temperatura > 30){
+            teste = "Temperatura acima do ideal, abaixe-a";
+            Serial.print("Temperatura: ");
+            Serial.print(temperatura);
+            Serial.print(" " + teste);
+            Serial.print(", ");
+            Serial.println(registro);
+          }
+          else{
+            if(temperatura < 25){
+              teste = "Temperatura abaixo do ideal, aumente-a";
+              Serial.print("Temperatura: ");
+              Serial.print(temperatura);
+              Serial.print(" " + teste);
+              Serial.print(", ");
+              Serial.println(registro);
+              }
+            }
+          }
+        }
+      }
+      if(registro > 100){
+        registro = 0;
+        Serial.println("ROW,SET,2");   
+      }
+      delay(3000);
+}
+
